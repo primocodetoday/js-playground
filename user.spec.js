a5614e8eec831d05e1b36cef677f5facbec7c768
primocodetoday <primocodetoday@gmail.com>
2020-09-04 11:47:19 +0200

    Added Jest configuration and eslint jest plugin

---
+++ user.spec.js
@@ -0,0 +1,8 @@
+﻿import User from './user';
+
+describe('User', () => {
+  test('name return full name', () => {
+    const user = new User({ firstName: 'Jan', lastName: 'Nowak' });
+    expect(user.name).toBe('Jan Nowak');
+  });
+});
