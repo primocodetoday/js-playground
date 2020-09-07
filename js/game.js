a5303b56e01e469829e14fe681a79d7f97906d57
primocodetoday <primocodetoday@gmail.com>
2020-09-07 23:21:35 +0200

    Basic class construction with drawLetters

--- js/game.js
+++ js/game.js
@@ -1,1 +1,52 @@
-﻿console.log('hello world');
+﻿class Game {
+  quotes = [
+    {
+      text: 'pan tadeusz',
+      category: 'Utwór literacki',
+    },
+    {
+      text: 'ogniem i mieczem',
+      category: 'Utwór literacki',
+    },
+    {
+      text: 'dzien niepodległosci',
+      category: 'Film',
+    },
+  ]
+
+  constructor({ outputWrapper, wordWrapper, categoryWrapper, lettersWrapper }) {
+    this.outputWrapper = outputWrapper
+    this.wordWrapper = wordWrapper
+    this.categoryWrapper = categoryWrapper
+    this.lettersWrapper = lettersWrapper
+
+    const { text, category } = this.quotes[
+      Math.floor(Math.random() * this.quotes.length)
+    ]
+    this.categoryWrapper.innerHTML = category
+  }
+
+  guess(letter) {}
+
+  drawLetters() {
+    for (let index = 0; index < 26; index += 1) {
+      const label = (index + 10).toString(36)
+      const button = document.createElement('button')
+      button.innerHTML = label
+      button.addEventListener('click', () => this.guess(label))
+      this.lettersWrapper.appendChild(button)
+    }
+  }
+
+  start() {
+    this.drawLetters()
+  }
+}
+
+const game = new Game({
+  outputWrapper: document.getElementById('output'),
+  wordWrapper: document.getElementById('word'),
+  categoryWrapper: document.getElementById('category'),
+  lettersWrapper: document.getElementById('letters'),
+})
+game.start()
