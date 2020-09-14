c82beb310285e268774532c38bede45703e83cab
primocodetoday <primocodetoday@gmail.com>
2020-09-14 10:12:41 +0200

    Dev: Generating Cells logic

---
+++ src/scripts/Cell.js
@@ -0,0 +1,14 @@
+﻿/* eslint-disable import/prefer-default-export */
+export class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.value = 0;
+    this.isMine = false;
+    this.isReveal = false;
+    this.isFlagged = false;
+    this.selector = `[data-x="${this.x}"][data-y="${this.y}"]`;
+    this.element = null;
+  }
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,57 @@
-﻿console.log('heloo');
+﻿import { Cell } from './Cell';
+
+class Game {
+  #config = {
+    easy: {
+      rows: 8,
+      cols: 8,
+      mines: 10,
+    },
+    medium: {
+      rows: 16,
+      cols: 16,
+      mines: 40,
+    },
+    expert: {
+      rows: 16,
+      cols: 30,
+      mines: 99,
+    },
+  };
+
+  #numberOfRows = null;
+  #numberOfCols = null;
+  #numberOfMines = null;
+  #cells = [];
+
+  initializeGame() {
+    this.#newGame();
+  }
+
+  #newGame(
+    rows = this.#config.easy.rows,
+    cols = this.#config.easy.cols,
+    mines = this.#config.easy.mines,
+  ) {
+    this.#numberOfRows = rows;
+    this.#numberOfCols = cols;
+    this.#numberOfMines = mines;
+
+    this.#generateCells();
+  }
+
+  #generateCells() {
+    for (let row = 0; row < this.#numberOfRows; row++) {
+      this.#cells[row] = [];
+      for (let col = 0; col < this.#numberOfCols; col++) {
+        this.#cells[row].push(new Cell(col, row));
+      }
+    }
+  }
+}
+
+window.onload = () => {
+  const game = new Game();
+
+  game.initializeGame();
+};
