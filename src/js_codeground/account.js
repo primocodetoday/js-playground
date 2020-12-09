7d6f722afa6f50666ec0dccd34ee3fa35c9787a0
primocodetoday <primocodetoday@gmail.com>
2020-12-09 12:50:42 +0100

    dev: DOM methods refresh

---
+++ src/js_codeground/account.js
@@ -0,0 +1,29 @@
+﻿const account = {
+  name: 'Sebastian',
+  incomes: [],
+  expenses: [],
+  addExpense(name, amount) {
+    this.expenses.push({ name, amount });
+  },
+  addIncome(name, amount) {
+    this.incomes.push({ name, amount });
+  },
+  getAccountSummary() {
+    let totalExp = 0;
+    let totalInc = 0;
+
+    this.expenses.forEach((expense) => {
+      totalExp += expense.amount;
+    });
+    this.incomes.forEach((income) => {
+      totalInc += income.amount;
+    });
+    return `${this.name} have total ${totalInc - totalExp}$ balance`;
+  },
+};
+
+account.addExpense('Car', 500);
+account.addExpense('Rent', 400);
+account.addIncome('Job', 1100);
+console.log(account);
+console.log(account.getAccountSummary());
---
+++ src/js_codeground/todoApp/todos.js
@@ -0,0 +1,34 @@
+﻿const todos = [
+  {
+    text: 'Cleanup an apartment',
+    completed: false,
+  },
+  {
+    text: 'Sweep the floor',
+    completed: false,
+  },
+  {
+    text: 'Fill up the car',
+    completed: true,
+  },
+  {
+    text: 'Take a walk',
+    completed: true,
+  },
+  {
+    text: 'Buy bread',
+    completed: false,
+  },
+];
+
+const uncompleted = todos.filter((el) => !el.completed);
+
+const summary = document.createElement('p');
+summary.textContent = `You have ${uncompleted.length} todos !`;
+document.body.appendChild(summary);
+
+todos.forEach((element) => {
+  const todo = document.createElement('p');
+  todo.textContent = `N: ${element.text}`;
+  document.body.appendChild(todo);
+});
